d589ce7704cdba15699f7ee563d5fac1e50e2e67
emalo <elie.jean.malo@gmail.com>
2024-08-14 11:39:27 +0000

    debut de gestion websockets chats rien encore fait

--- backend/pkg/models/chats.go
+++ backend/pkg/models/chats.go
@@ -13,3 +13,18 @@ type Chat struct {
 	Msg        string
 	CreatedAt  time.Time
 }
+
+var clientWebSocketConnections = make(map[int]*websocket.Conn)
+
+func WebsocketService(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("cookieName")
+	if err != nil {
+		return
+	}
+	sender := GetSender(cookie.Name)
+	fmt.Println("sender name: ", sender)
+}
+
+func GetSender(cookie string) int {
+	return 0
+}

304b752f16101eea56cba76175d8a6c84bd0ca19
sefaye <wiwadev@gmail.com>
2024-08-17 00:54:07 +0000

    creation d'une table notif.. pour les notification

--- backend/pkg/db/migrations/sqlite/000009_create_notifications_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000009_create_notifications_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Notifications (
     id TEXT PRIMARY KEY,
     user_receiver_id TEXT NOT NULL,  -- Référence à l'utilisateur qui reçoit la notification
-    user_id TEXT NOT NULL       -- Référence à l'utilisateur qui envoie la notification
+    user_id TEXT NOT NULL,       -- Référence à l'utilisateur qui envoie la notification
     type TEXT NOT NULL,        -- Type de notification (par exemple, 'follow_request', 'group_invitation', 'event_created', etc.)
     message TEXT,              -- Message de notification ou détails
     is_read BOOLEAN DEFAULT FALSE, -- Indique si la notification a été lue

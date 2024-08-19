dd07e3465d48440cc1642a58aa06097f5b31088a
sefaye <wiwadev@gmail.com>
2024-08-19 15:34:29 +0000

    notification service done

--- backend/pkg/models/notifications.go
+++ backend/pkg/models/notifications.go
@@ -1,14 +1,19 @@
 package models
 
-import "github.com/google/uuid"
-
-
-
-//\\ A revoir //\\
-
 type Notification struct {
-	ID         uuid.UUID      `json:"id"`
-	ReceiverID uuid.UUID `json:"receiver_id"`
-	SenderID   uuid.UUID `json:"sender_id"`
-	Count      uuid.UUID      `json:"count"`
+	Id         string
+	ReceiverID string
+	SenderID   string
+	Type       string
+	Message    string
+	IsRead     bool
+	CreateAt   string
+}
+
+type MessageNotif struct {
+	Type       string
+	Content    string
+	SenderId   string
+	ReceiverId string
+	TypeOf     string
 }

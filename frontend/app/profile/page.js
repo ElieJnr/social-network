6e46e92c9df20d9aab20c8472e246c5ae5241577
mbadiao <diaombaye832@gmail.com>
2024-08-27 12:08:35 +0000

    remove api file and create server actions

--- frontend/app/profile/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>page protected</div>
-  )
-}
-
-export default page
--- frontend/components/ui/card.jsx
+++ frontend/components/ui/card.jsx
@@ -5,7 +5,11 @@ import { cn } from "@/lib/utils"
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
+<<<<<<< HEAD
     className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+=======
+    className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}
+>>>>>>> 32795fc (add middleware to the front)
     {...props} />
 ))
 Card.displayName = "Card"
@@ -21,7 +25,11 @@ CardHeader.displayName = "CardHeader"
 const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
   <h3
     ref={ref}
+<<<<<<< HEAD
     className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
+=======
+    className={cn("font-semibold leading-none tracking-tight", className)}
+>>>>>>> 32795fc (add middleware to the front)
     {...props} />
 ))
 CardTitle.displayName = "CardTitle"
